eb432c92f9ec60c63c60f62586a194adc06396e9
mkishine <mkishine@yahoo.com>
2015-03-30 23:40:57 -0400

    page 172

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 'use strict';
-angular.module("sportsStore", ["customFilters", "cart"]);
+angular.module("sportsStore", ["customFilters", "cart", "ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider.when("/complete", {
+            templateUrl: "views/thankYou.html"
+        });
+        $routeProvider.when("/placeorder", {
+            templateUrl: "views/placeOrder.html"
+        });
+        $routeProvider.when("/checkout", {
+            templateUrl: "views/checkoutSummary.html"
+        });
+        $routeProvider.when("/products", {
+            templateUrl: "views/productList.html"
+        });
+        $routeProvider.otherwise({
+            templateUrl: "views/productList.html"
+        });
+    });
