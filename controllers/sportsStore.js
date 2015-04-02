140d5fa711b0b096ac633c829b2378de604bffe6
mkishine <mkishine@yahoo.com>
2015-04-01 21:04:42 -0400

    Added sendOrder method

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,18 +1,32 @@
 'use strict';
 angular.module("sportsStore")
-	.constant("dataUrl","http://localhost:5500/products")
-	.controller("sportsStoreCtrl",function($scope, $http, dataUrl) {
-			// console.log("Hello");
-			$scope.data = {};
-			$http.get(dataUrl)
-			.success(function(data) {
-				// console.log("Data");;
-				$scope.data.products = data;
-			})
-			.error(function(data, status) {
-				// console.log("Error");
-				// debugger;
-				$scope.data.error = { "status" : status };
-				// debugger;
-			});
-		});
+    .constant("dataUrl", "http://localhost:5500/products")
+    .constant("orderUrl", "http://localhost:5500/orders")
+    .controller("sportsStoreCtrl", function ($scope, $http, $location, dataUrl, orderUrl, cart)
+{
+    $scope.data = {};
+    $http.get(dataUrl)
+        .success(function (data) {
+            $scope.data.products = data;
+        })
+        .error(function (data, status) {
+            $scope.data.error = {"status": status};
+        });
+
+    $scope.sendOrder = function (shippingDetails) {
+        var order = angular.copy(shippingDetails);
+        order.products = cart.getProducts();
+        $http.post(orderUrl, order)
+            .success(function (data) {
+                $scope.data.orderId = data.id;
+                cart.getProducts().length = 0;
+            })
+            .error(function (error) {
+                $scope.data.orderError = error;
+            }).finally(function () {
+                $location.path("/complete");
+            });
+    };
+}
+)
+;
