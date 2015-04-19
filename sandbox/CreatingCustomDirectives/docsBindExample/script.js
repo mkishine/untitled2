de42544a1140e19572b628dd2e2cb23afb188a53
mkishine <mkishine@yahoo.com>
2015-04-19 13:43:37 -0400

    Creating Custom Directives examples

---
+++ sandbox/CreatingCustomDirectives/docsBindExample/script.js
@@ -0,0 +1,5 @@
+angular.module('docsBindExample', [])
+    .controller('Ctrl1', function Ctrl1($scope) {
+        $scope.name = 'Max Karl Ernst Ludwig Planck (April 23, 1858 - October 4, 1947)';
+        $scope.hello = 'world';
+    });
