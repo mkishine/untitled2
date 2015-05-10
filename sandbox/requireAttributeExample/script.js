8c658306f3c7d4f276dd95cab8f45e09a199d8b1
mkishine <mkishine@yahoo.com>
2015-05-10 17:51:08 -0400

    Finally got buttons talk to each other

--- sandbox/requireAttributeExample/script.js
+++ sandbox/requireAttributeExample/script.js
@@ -1,31 +1,65 @@
 angular.module('t2', [])
     .controller('ctrl', ['$scope', function($scope) {
-        $scope.l1 = "hello";
-        $scope.l2 = "world";
+        $scope.hello = "hello";
+        $scope.hi = "hi";
+        $scope.earth = "earth";
+        $scope.fire = "fire";
+        $scope.water = "water";
     }])
-    .directive('myButton', function() {
+    .directive('x', function() {
         return {
-            require: '^myName',
             restrict: 'C',
-            scope: {
-                label: '=',
-            },
-            link: function (scope, element, attrs, nameCtrl) {
-                element.text(scope.label);
-                element[0].onclick = function(){
-                    console.log(scope.label + " clicked");
-                    nameCtrl.sayName(scope.label);
+            controller: function($scope) {
+                $scope.listeners = [];
+                this.register = function(listener) {
+                    $scope.listeners.push(listener);
+                };
+                this.notify = function(label) {
+                    // console.log("x notified: "+label);
+                    $scope.listeners.forEach(function(value){
+                        value.scope.notify(value.scope, value.element, this);
+                    }, label);
                 };
             }
         };
     })
-    .directive('myName', function() {
+    .directive('greetingButton', function() {
         return {
+            require: '^x',
             restrict: 'C',
             controller: function($scope) {
-                this.sayName = function(label) {
-                    console.log("my name is "+label);
-                }
+                $scope.notify = function(scope, element, label){
+                    // console.log("Greeting notified: "+label);
+                    element.text("Greeting: "+scope.label + " ("+scope.label +" "+label+")");
+                };
+            },
+            scope: {
+                label: '=',
+            },
+            link: function (scope, element, attrs, xCtrl) {
+                element.css({border: "5px solid", cursor: "pointer", margin: "5px"});
+                element.text("Greeting: "+scope.label);
+                xCtrl.register({scope: scope, element: element});
+                element[0].onclick = function(){
+                    console.log(scope.label + " clicked");
+                };
+            }
+        };
+    })
+    .directive('elementButton', function() {
+        return {
+            require: '^x',
+            restrict: 'C',
+            scope: {
+                label: '=',
+            },
+            link: function (scope, element, attrs, xCtrl) {
+                element.css({border: "5px solid", cursor: "pointer", margin: "5px"});
+                element.text("Element: "+scope.label);
+                element[0].onclick = function(){
+                    // console.log(scope.label + " clicked");
+                    xCtrl.notify(scope.label);
+                };
             }
         };
     })
