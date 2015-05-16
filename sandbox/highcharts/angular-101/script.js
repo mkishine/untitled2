b0a5d5d3ab6a44cc80db0512286f6efd0b00c037
mkishine <mkishine@yahoo.com>
2015-05-15 21:25:45 -0400

    propagated scatter zooming to MyCtrl

--- sandbox/highcharts/angular-101/script.js
+++ sandbox/highcharts/angular-101/script.js
@@ -42,12 +42,15 @@ angular.module('myApp', [])
             $scope.reportCounts = h2p(counter("report"));
             $scope.userTimes = h2p(counter("user", "req_time"));
             $scope.reportTimes = h2p(counter("report", "req_time"));
-
+            $scope.scatterZoomed = function(xMin, xMax){
+                console.log("MyCtrl.scatterZoomed: "+xMin+" "+xMax);
+            };
         });
     })
     .directive('hcCoordinator', function() {
         return {
             restrict: 'C',
+            transclude: true,
             controller: function($scope) {
                 $scope.scatterPlot = {};
                 this.registerScatterPlot = function(scatterPlot) {
@@ -55,6 +58,9 @@ angular.module('myApp', [])
                 };
                 this.pieClicked = function(name, category, color) {
                     $scope.scatterPlot.scope.notify(name, category, color);
+                };
+                this.scatterZoomed = function(xMin, xMax){
+                    $scope.scatterZoomed(xMin, xMax);
                 };
             }
         };
@@ -179,11 +185,9 @@ angular.module('myApp', [])
                             zoomType: 'x',
                             events: {
                                 selection: function (event) {
-                                    if (event.xAxis) {
-                                        console.log("zoomed: " + event.xAxis[0].min + " " + event.xAxis[0].max);
-                                    } else {
-                                        console.log('Selection reset');
-                                    }
+                                    var xMin = event.xAxis ? event.xAxis[0].min : undefined;
+                                    var xMax = event.xAxis ? event.xAxis[0].max : undefined;
+                                    coordinator.scatterZoomed(xMin, xMax);
                                 }
                             }
                         },
